80b42d8a047f13f2ff0a6919753b296917202d72
guilhermesdias45 <guilherme.sdias@sptech.school>
2025-08-29 14:59:10 -0300

    Exercícios finalizados

--- src/main/java/school/sptech/Main.java
+++ src/main/java/school/sptech/Main.java
@@ -4,5 +4,53 @@ public class Main {
 
     public static void main(String[] args) {
 
+        Time time = new Time("Rocha Sólida");
+        time.registrarVitoria();
+        time.registrarVitoria();
+        time.registrarVitoria();
+        time.registrarVitoria();
+
+        time.registrarEmpate();
+        time.registrarEmpate();
+        time.registrarEmpate();
+
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+        time.registrarDerrota();
+
+        time.exibirInformacoes();
+
+        Time time2 = new Time("Fogo Ardente");
+
+        time2.registrarVitoria();
+        time2.registrarVitoria();
+        time2.registrarVitoria();
+        time2.registrarVitoria();
+
+        time2.registrarEmpate();
+        time2.registrarEmpate();
+        time2.registrarEmpate();
+        time2.registrarEmpate();
+        time2.registrarEmpate();
+
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+        time2.registrarDerrota();
+
+        time2.exibirInformacoes();
+
+        time.compararAproveitamento(time2);
     }
 }
--- src/main/java/school/sptech/Time.java
+++ src/main/java/school/sptech/Time.java
@@ -1,5 +1,68 @@
 package school.sptech;
 
+import java.text.MessageFormat;
+
 public class Time {
-.
+    String nome;
+    Integer vitorias = 0;
+    Integer empates = 0;
+    Integer derrotas = 0;
+
+    public Time(String nome){
+        this.nome = nome;
+        this.vitorias = (Integer) 0;
+        this.empates = (Integer) 0;
+        this.derrotas = (Integer) 0;
+    }
+
+    public void registrarVitoria(){
+        this.vitorias += 1;
+    }
+
+    public void registrarEmpate(){
+        this.empates += 1;
+    }
+
+    public void registrarDerrota(){
+        this.derrotas += 1;
+    }
+
+    public Integer getPontos(){
+        return (Integer) (this.vitorias * 3) + this.empates;
+    }
+
+    public Integer getTotalPartidas(){
+        return this.vitorias + this.empates + this.derrotas;
+    }
+
+    public Integer getAproveitamento(){
+        Integer totalPartidas = this.getTotalPartidas();
+        Integer vitorias = this.vitorias;
+        double resultado = ((double) vitorias/ totalPartidas)*100;
+
+        return (int) resultado;
+    }
+
+    public void compararAproveitamento(Time adversario){
+        Integer time = this.getAproveitamento();
+        Integer rival = adversario.getAproveitamento();
+
+        if (time == rival){
+            System.out.println("Os times " + this.nome + " e " + adversario.nome + " têm o mesmo aproveitamento");
+        } else if(time > rival){
+            System.out.println("O time " + this.nome + " tem um aproveitamento maior que o time " + adversario.nome);
+        } else{
+            System.out.println("O time " + adversario.nome + " tem um aproveitamento maior que o time " + this.nome);
+        }
+    }
+
+    public void exibirInformacoes(){
+        System.out.println("Time: " + this.nome);
+        System.out.println("Vitórias: " + this.vitorias);
+        System.out.println("Empates: " + this.empates);
+        System.out.println("Derrotas: " + this.derrotas);
+        System.out.println("\nPontos: " + this.getPontos());
+        System.out.println("Total de Partidas: " + this.getTotalPartidas());
+        System.out.println("Aproveitamento: " + this.getAproveitamento() + "%");
+    }
 }
